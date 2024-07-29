4e3993354f200b6ea418a6068f3bb43f7c4bbebc
Steffan <steffan.schiewe@icloud.com>
2024-07-29 13:02:13 +0200

    feat: add create plugin command

---
+++ src/CreatePluginCommand.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace YOOtheme\Starter;
+
+use Symfony\Component\Console\Attribute\AsCommand;
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Console\Question\Question;
+use Symfony\Component\Filesystem\Filesystem;
+use Symfony\Component\Filesystem\Path;
+use Symfony\Component\Finder\Finder;
+use YOOtheme\Starter\StringHelper as Str;
+
+#[AsCommand(name: 'create:plugin', description: 'Create a plugin')]
+class CreatePluginCommand extends Command
+{
+    protected string $stubs = __DIR__ . '/stubs';
+
+    protected function configure()
+    {
+        $this->addArgument('name', InputArgument::OPTIONAL, default: basename(getcwd()));
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output): int
+    {
+        $fs = new Filesystem();
+        $cwd = getcwd();
+
+        $name = $input->getArgument('name');
+        $path = Path::join($cwd, 'build');
+
+        $fn = [$this->getHelper('question'), 'ask'];
+        $ask = $this->partial($fn, $input, $output);
+        $finder = (new Finder())->in("{$this->stubs}/plugin");
+
+        $filemap = [
+            '/plugin.xml' => "/{$name}.xml",
+            '/plugin.stub' => "/{$name}.php",
+        ];
+
+        $variables = [
+            'NAME' => $name,
+            'PLUGIN' => $name,
+            'PLUGIN_CLASS' => 'plgSystem' . strtr($name, ['-' => '', '_' => '']),
+            'TITLE' => $ask(new Question('Enter plugin title: ', $name)),
+        ];
+
+        foreach ($finder->files() as $file) {
+            $fs->dumpFile(
+                strtr("{$path}/{$file->getRelativePathname()}", $filemap),
+                Str::placeholder($file->getContents(), $variables),
+            );
+        }
+
+        $output->writeln("Plugin '{$name}' created successfully.");
+
+        return Command::SUCCESS;
+    }
+
+    protected function partial(callable $func, ...$args): callable
+    {
+        return fn(...$rest) => $func(...$args, ...$rest);
+    }
+}
