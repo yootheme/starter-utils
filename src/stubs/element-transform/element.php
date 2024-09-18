cfdb3c3614197192daa9341b1899960ab0e370a5
hendrikbehncke <hendrik.behncke@gmx.de>
2024-09-18 18:02:46 +0200

    feat: add element transform example

---
+++ src/stubs/element-transform/element.php
@@ -0,0 +1,27 @@
+<?php
+
+// namespace
+
+return [
+    // Define transforms for the element node
+    'transforms' => [
+        // The function is executed before the template is rendered
+        'render' => function ($node, array $params) {
+            // Element object (stdClass)
+            $node->type; // Type name (string)
+            $node->props; // Field properties (array)
+            $node->children; // All children (array)
+
+            // Parameter array
+            $params['path']; // All parent elements (array)
+            $params['parent']; // Parent element (stdClass)
+            $params['builder']; // Builder instance (YOOtheme\Builder)
+            $params['type']; // Element definition (YOOtheme\Builder\ElementType)
+        },
+    ],
+    // Define updates for the element node
+    'updates' => [
+        // The function is executed if the YOOtheme Pro version, when the element was saved, is smaller than the current version.
+        /*'1.18.0' => function ($node, array $params) {},*/
+    ],
+];
